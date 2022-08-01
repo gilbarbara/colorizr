3e4c0804f18486805bbeec567082b16b0d3d8dd3
Gil Barbara <gilbarbara@gmail.com>
2022-08-01 11:10:17 -0300

    Update demo

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(<App />, rootElement);
+}
