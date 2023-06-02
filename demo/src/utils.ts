ad346f35bbc06ebd65964a3f34466825a2e042b8
Gil Barbara <gilbarbara@gmail.com>
2023-06-02 01:55:16 -0300

    Update demo

---
+++ demo/src/utils.ts
@@ -0,0 +1,39 @@
+export interface ContrastProps {
+  contrast: number;
+  largeText: number;
+  normalText: number;
+}
+
+export function getColorNumber(index: number) {
+  return index === 0 ? 50 : index * 100;
+}
+
+export function getContrastBgColor({ contrast }: ContrastProps) {
+  let backgroundColor = '#fbd0da';
+
+  if (contrast >= 7) {
+    backgroundColor = '#d2fbd0';
+  } else if (contrast >= 4.5) {
+    backgroundColor = '#f5f5d0';
+  }
+
+  return backgroundColor;
+}
+
+export function getKey(value: string, index: number) {
+  return `${value}-${index}`;
+}
+
+export function getTextBgColor(type: 'normal' | 'large') {
+  return ({ largeText, normalText }: ContrastProps) => {
+    let backgroundColor = '#fbd0da';
+
+    if (type === 'normal' ? normalText === 2 : largeText === 2) {
+      backgroundColor = '#d2fbd0';
+    } else if (type === 'normal' ? normalText === 1 : largeText === 1) {
+      backgroundColor = '#f5f5d0';
+    }
+
+    return backgroundColor;
+  };
+}
