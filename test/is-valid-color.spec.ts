7a75ff4ab2b8009934d200f46da038794355609f
Gil Barbara <gilbarbara@gmail.com>
2025-12-26 14:17:01 -0300

    Add support for angle units and 'none' keyword in color parsing

--- test/is-valid-color.spec.ts
+++ test/is-valid-color.spec.ts
@@ -15,6 +15,19 @@ describe('isValidColor', () => {
     [addOpacityToCssString(violet.oklchString, 90, true), true],
     ['oklch(75% 49% 180)', true],
     ['oklab(50% 25% -30%)', true],
+    // Angle units for HSL
+    ['hsl(120deg 100% 50%)', true],
+    ['hsl(0.5turn 100% 50%)', true],
+    ['hsl(3.14rad 100% 50%)', true],
+    ['hsl(200grad 100% 50%)', true],
+    // Angle units for OkLCH
+    ['oklch(0.5 0.2 180deg)', true],
+    ['oklch(0.5 0.2 0.5turn)', true],
+    // none keyword (CSS Color Level 4)
+    ['rgb(none 128 255)', true],
+    ['hsl(none 50% 50%)', true],
+    ['oklab(none 0.1 -0.1)', true],
+    ['oklch(0.5 none 180)', true],
     [yellow.rgbString, true],
     [addOpacityToCssString(yellow.rgbString, 0.9), true],
     [addOpacityToCssString(yellow.rgbString, 90, true), true],
